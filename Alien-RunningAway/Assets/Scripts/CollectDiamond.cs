c69d4379dd489fed21be478400ec3c8261277a38
esraakblt <esraakbulut1@outlook.com>
2023-06-03 23:10:54 +0300

    diamond toplama yapıldı

--- Alien-RunningAway/Assets/Scripts/CollectDiamond.cs
+++ Alien-RunningAway/Assets/Scripts/CollectDiamond.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class CollectDiamond : MonoBehaviour
 {
+    public int score;
+    public TextMeshProUGUI DiamondText;
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Diamond"))
         {
+            AddDiamond();
             Destroy(other.gameObject);
         }
+    }
+
+
+    public void AddDiamond()
+    {
+        score++;
+        DiamondText.text = "Score:" + score.ToString(); 
     }
 }
