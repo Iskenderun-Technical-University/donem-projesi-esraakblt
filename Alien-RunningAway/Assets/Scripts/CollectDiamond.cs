5249ced4318c4f05b73d3ecf5f8a6f5e9a4e18fc
esraakblt <esraakbulut1@outlook.com>
2023-06-04 16:49:50 +0300

    Engel ve canavarlar eklendi animasyonları yapıldı

--- Alien-RunningAway/Assets/Scripts/CollectDiamond.cs
+++ Alien-RunningAway/Assets/Scripts/CollectDiamond.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+
+
 public class CollectDiamond : MonoBehaviour
 {
     public int score;
     public TextMeshProUGUI DiamondText;
+    public PlayerController playerController;
 
 
     private void OnTriggerEnter(Collider other)
@@ -15,8 +19,23 @@ public class CollectDiamond : MonoBehaviour
             AddDiamond();
             Destroy(other.gameObject);
         }
+        else if (other.CompareTag("End"))
+        {
+            Debug.Log("Congrats!..");
+            playerController.runningSpeed = 0;
+            //Oyun sonuna gelince hýz 0 oluyor ve congrats yazýsý consoleda görünür.
+
+        }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Collision"))
+        {
+            Debug.Log("Touch Obstacle!..");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Engele çarpýnca oyun yeniden yükleniyor
+        }
+    }
 
     public void AddDiamond()
     {
