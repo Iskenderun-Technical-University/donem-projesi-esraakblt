eed540e9c280867873c188cc8e62491b19daa098
esraakblt <esraakbulut1@outlook.com>
2023-06-04 20:11:50 +0300

    Oyun düzenlendi restart ve oyuncu hareketleri eklendi

--- Alien-RunningAway/Assets/Scripts/CollectDiamond.cs
+++ Alien-RunningAway/Assets/Scripts/CollectDiamond.cs
@@ -10,7 +10,17 @@ public class CollectDiamond : MonoBehaviour
     public int score;
     public TextMeshProUGUI DiamondText;
     public PlayerController playerController;
+    public int maxScore;
+    public Animator PlayerAnim;
+    public GameObject Player;
+    public GameObject EndPanel;
 
+
+
+    private void Start()
+    {
+          PlayerAnim = Player.GetComponentInChildren<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -24,11 +34,29 @@ public class CollectDiamond : MonoBehaviour
             Debug.Log("Congrats!..");
             playerController.runningSpeed = 0;
             //Oyun sonuna gelince hýz 0 oluyor ve congrats yazýsý consoleda görünür.
+            transform.Rotate(transform.rotation.x,180,transform.rotation.z,Space.Self);
+            EndPanel.SetActive(true);
+            //karakter bize dönecek 
+            if (score >= maxScore)
+            {
+               // Debug.Log("You Win!..");
+                PlayerAnim.SetBool("Win",true);
+            }
+            else
+            {
+               // Debug.Log("You Lose!..");
+                PlayerAnim.SetBool("Lose",true);
+            }
 
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+        private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Collision"))
         {
